437eec50c59485bb898e42d659b0b7eb5415aed8
Anjani Prajapati <anjaniprajapati21@navgurukul.org>
2023-06-30 23:51:27 +0530

    first commit

---
+++ app/Componet/Header.jsx
@@ -0,0 +1,17 @@
+var Head = ()=>{
+    return (
+        <div className="Row">
+            <div className="row_1"></div>
+            <div className="row">
+                <img src="public/icon-Logo.jpg" className="Icon" alt="logo"></img>
+                <p className="movieHub">Movie Hub</p>
+            </div>
+            <div className="row_1"></div>
+            <div className="Row_1"></div>
+        </div>
+        
+
+        
+    )
+}
+export default Head;
---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import Head from './Componet/Header'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <Head />
+  )
+}
